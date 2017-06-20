30d9ad3910991c569223a408e9e7135129538a5f
Energy1190 <energyneo0@gmail.com>
2017-06-20 14:59:09 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -36,12 +36,6 @@ fi
 
 rm -f /etc/nsswitch.conf
 cat <<EOF > /etc/nsswitch.conf
-# /etc/nsswitch.conf
-#
-# Example configuration of GNU Name Service Switch functionality.
-# If you have the `glibc-doc-reference' and `info' packages installed, try:
-# `info libc "Name Service Switch"' for information about this file.
-
 passwd:         compat winbind
 group:          compat winbind
 shadow:         compat winbind
